54a6d1e9b8c0eb3fdb5c6d00ae2da0911ef28b63
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-11-11 10:39:41 +0000

    design is responsive now

--- app/fonts/_components/PersonalCard.jsx
+++ app/fonts/_components/PersonalCard.jsx
@@ -4,17 +4,20 @@ import NameCard from './_atoms/NameCard'
 
 const PersonalCard = () => {
   return (
-    <div className="w-9/12 h-96 bg-slate-300 rounded-xl group">
-    <div className="w-full h-36 rounded-t-xl relative">
-      <div className="w-full h-full bg-[url('/linkedinheader.jpeg')] rounded-t-xl"></div>
-        <div className="w-36 h-36 bg-slate-400 rounded-full absolute -bottom-1/2 left-6">
+    <div className="w-full md:w-10/12 lg:w-8/12 h-96 bg-slate-300 md:rounded-xl group mx-auto">
+    <div className="w-full h-48 rounded-t-xl relative">
+        <div 
+            className="w-full h-full bg-[url('/linkedinheader.jpeg')] bg-cover bg-center md:rounded-t-xl">
+        </div>
+        <div className="w-24 h-24 md:w-36 md:h-36 bg-slate-400 rounded-full absolute -bottom-1/2 left-1/2 transform -translate-x-1/2 md:left-6 md:translate-x-0">
             <ProfilePhoto/>
         </div>
     </div>
     <div className="bg-white w-full h-56 flex flex-col">
-        <div className="mt-24 ml-6"><NameCard/></div>
+        <div className="mt-16 md:mt-24 ml-6 text-center md:text-left"><NameCard/></div>
     </div>
 </div>
+
 
   )
 }
